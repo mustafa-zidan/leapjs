ff0be44f1012b9ef5ab53842c7571a0f5411c588
deckar01 <deckar01@nsuok.edu>
2013-06-09 00:20:53 -0600

    New custom template

---
+++ tree.js
@@ -0,0 +1,77 @@
+var tree = [
+	["Leap JavaScript API Reference", "class_index.html", "", [
+		[
+	"Leap", 
+	"Leap.html", "", [
+  [
+	"CircleGesture", 
+	"Leap.CircleGesture.html", " < Leap.Gesture", [
+  
+	]
+]
+, [
+	"Controller", 
+	"Leap.Controller.html", "", [
+  
+	]
+]
+, [
+	"Frame", 
+	"Leap.Frame.html", "", [
+  
+	]
+]
+, [
+	"Gesture", 
+	"Leap.Gesture.html", "", [
+  
+	]
+]
+, [
+	"Hand", 
+	"Leap.Hand.html", "", [
+  
+	]
+]
+, [
+	"KeyTapGesture", 
+	"Leap.KeyTapGesture.html", " < Leap.Gesture", [
+  
+	]
+]
+, [
+	"Matrix", 
+	"Leap.Matrix.html", "", [
+  
+	]
+]
+, [
+	"Pointable", 
+	"Leap.Pointable.html", "", [
+  
+	]
+]
+, [
+	"ScreenTapGesture", 
+	"Leap.ScreenTapGesture.html", " < Leap.Gesture", [
+  
+	]
+]
+, [
+	"SwipeGesture", 
+	"Leap.SwipeGesture.html", " < Leap.Gesture", [
+  
+	]
+]
+, [
+	"Vector", 
+	"Leap.Vector.html", "", [
+  
+	]
+]
+
+	]
+]
+
+	]]
+];
